d58bb3d0378d86f16a7a4c57ee30a768e0a13088
Prapti Patil <praptipatil0804@gmail.com>
2023-03-10 14:48:33 +0530

    Spring Security implemented

---
+++ src/main/java/com/example/demo/config/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package com.example.demo.config;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.entity.Employee;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+	
+	private Employee employee;
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return employee.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return employee.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+
+}
